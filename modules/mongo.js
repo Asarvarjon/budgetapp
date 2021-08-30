4f2c364741f41444a2f5b167a2a7a7e86ba7f06f
Amriyev Sarvarjon <80247784+Asarvarjon@users.noreply.github.com>
2021-08-30 11:31:59 +0500

    updated

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb")
 
- const mongoAtlasUrl = 'mongodb://localhost:27017'
+ const mongoAtlasUrl = 'mongodb+srv://mongo:mongo@cluster0.uvl2x.mongodb.net/usersystem?retryWrites=true&w=majority'
 
  const client = new MongoClient(mongoAtlasUrl);
 
